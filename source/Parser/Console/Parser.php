172d7c642e960d86d60381635c729fcd57e88bf7
Ilya S <public.ilya.s@gmail.com>
2014-05-10 20:56:13 +0700

    awesome refactoring

--- source/Parser/Console/Parser.php
+++ source/Parser/Console/Parser.php
@@ -3,6 +3,13 @@
 class Parser {
 
     /**
+     * Each command gets splitted into chunks.
+     *
+     * @var array
+     */
+    protected $chunks = [];
+
+    /**
      * Parse a console command into an array.
      *
      * @param string $command
@@ -10,13 +17,34 @@ class Parser {
      */
     public function parse($command)
     {
-        $chunks = explode(' ', $command);
+        $this->splitIntoChunks($command);
 
         return [
-            'name' => array_shift($chunks) ?: null,
-            'arguments' => $chunks,
-            'options' => []
+            'name'      => $this->extractName(),
+            'arguments' => $this->chunks,
+            'options'   => []
         ];
+    }
+
+    /**
+     * Split the given string into "chunks".
+     *
+     * @param string $string
+     * @return void
+     */
+    protected function splitIntoChunks($string)
+    {
+        $this->chunks = array_filter(explode(' ', $string));
+    }
+
+    /**
+     * Extract a command name from the list of chunks.
+     *
+     * @return string|null
+     */
+    protected function extractName()
+    {
+        return array_shift($this->chunks);
     }
 
 }
