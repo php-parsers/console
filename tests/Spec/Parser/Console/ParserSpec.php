1e63afdb96465fa1719876f8d586e587baf4176f
Ilya S <public.ilya.s@gmail.com>
2014-05-10 20:36:38 +0700

    refactor just a touch

--- tests/Spec/Parser/Console/ParserSpec.php
+++ tests/Spec/Parser/Console/ParserSpec.php
@@ -11,17 +11,22 @@ class ParserSpec extends ObjectBehavior {
 
     function it_parses_a_console_command_into_an_array()
     {
-        $this->parse('')->shouldBe([
+        $this->parse('')->shouldBe($this->merge());
+
+        $this->parse('foo')->shouldBe($this->merge([
+            'name' => 'foo'
+        ]));
+    }
+
+    protected function merge(array $data = [])
+    {
+        $structure = [
             'name' => null,
             'arguments' => [],
             'options' => []
-        ]);
+        ];
 
-        $this->parse('foo')->shouldBe([
-            'name' => 'foo',
-            'arguments' => [],
-            'options' => []
-        ]);
+        return array_merge($structure, $data);
     }
 
 }
